7188fdbaf0219d00a4615cf0389a731a1eca8282
lioncat2002 <sankhayan2002@gmail.com>
2024-02-23 02:52:23 +0530

    refactor: make doctor route un protected

--- routes/doctor.go
+++ routes/doctor.go
@@ -2,14 +2,12 @@ package routes
 
 import (
 	"telmed_backend/controllers"
-	"telmed_backend/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func doctorRouter(c fiber.Router) {
 	c.Post("", controllers.CreateDoctor)
-	c.Use(middlewares.CheckAuth)
 	c.Get("", controllers.FetchAllDoctor)
 	c.Get("/:doctor_id", controllers.FetchDoctor)
 
